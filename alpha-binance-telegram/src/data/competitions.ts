6d9f6b9ad3b39317e81e498a83f81c084caca64d
Co Tran <tranbinhco99@gmail.com>
2025-08-01 17:37:19 +0700

    update compe

--- alpha-binance-telegram/src/data/competitions.ts
+++ alpha-binance-telegram/src/data/competitions.ts
@@ -29,17 +29,18 @@ export const competitionsData: Competition[] = [
     // { id: 26, tokenName: 'CROSS', condition: 'Top 11000', deadline: '24/07/2025 23:00:00', reward: 909 },
     // { id: 27, tokenName: 'ECHO', condition: 'Top 5000', deadline: '24/07/2025 23:00:00', reward: 2000 },
     // { id: 28, tokenName: 'PEAQ', condition: 'Top 11000', deadline: '26/07/2025 14:00:00', reward: 955 },
-    { id: 29, tokenName: 'SPA', condition: 'Top 9000', deadline: '27/07/2025 21:00:00', reward: 4900 },
-    { id: 30, tokenName: 'UPTOP', condition: 'Top 5580', deadline: '03/08/2025 14:00:00', reward: 1700 },
+    // { id: 29, tokenName: 'SPA', condition: 'Top 9000', deadline: '27/07/2025 21:00:00', reward: 4900 },
     { id: 31, tokenName: 'SUNDOG', condition: 'Top 11300', deadline: '31/07/2025 23:00:00', reward: 1111 },
     { id: 32, tokenName: 'NFT', condition: 'Top 11300', deadline: '31/07/2025 23:00:00', reward: 155474173 },
     { id: 33, tokenName: 'PEPEONTRON', condition: 'Top 11300', deadline: '31/07/2025 23:00:00', reward: 990 },
     { id: 34, tokenName: 'TRX', condition: 'Top 11300', deadline: '31/07/2025 23:00:00', reward: 230 },
+    { id: 30, tokenName: 'UPTOP', condition: 'Top 5580', deadline: '03/08/2025 14:00:00', reward: 1700 },
     { id: 35, tokenName: 'VRA', condition: 'Top 15000', deadline: '10/08/2025 15:00:00', reward: 64000 },
     { id: 36, tokenName: 'ASP', condition: 'Top 14000', deadline: '13/08/2025 23:00:00', reward: 428 },
     { id: 37, tokenName: 'ZRC', condition: 'Top 14000', deadline: '13/08/2025 23:00:00', reward: 1785 },
     { id: 38, tokenName: 'YALA', condition: 'Top 14000', deadline: '13/08/2025 23:00:00', reward: 357 },
     { id: 39, tokenName: 'TA', condition: 'Top 6000', deadline: '13/08/2025 23:00:00', reward: 1000 },
+    { id: 40, tokenName: 'MIA', condition: 'Top 6700', deadline: '15/08/2025 14:00:00', reward: 740 },
 ];
 
 export const alphaSoonData: AlphaSoon[] = [
@@ -61,6 +62,7 @@ export const alphaSoonData: AlphaSoon[] = [
     // { id: 14, tokenName: 'NAORIS', type: 'AIRDROP2', date: '31/07/2025 19:00:00', reward: '', points: '', tags: [1] },
     // { id: 142, tokenName: 'PLAY', type: 'AIRDROP2', date: '31/07/2025 15:00:00', reward: '', points: '', tags: [1] },
     { id: 141, tokenName: 'AIO', type: 'AIRDROP2', date: '02/08/2025 23:00:00', reward: '', points: '' },
+    { id: 143, tokenName: 'MM', type: 'AIRDROP2', date: '03/08/2025 23:00:00', reward: '', points: '' },
     { id: 142, tokenName: 'TOWNS', type: 'AIRDROP2', date: '05/08/2025 23:00:00', reward: '', points: '' },
     { id: 20, tokenName: 'BTR', type: 'AIRDROP2', date: '', reward: '', points: '' },
 ]; 
